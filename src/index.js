983a2fa69e0c4aa607f33ab631228c6713ae2d68
Joao Victor <joao.teixeira@globaldotcom.con.br>
2023-04-22 17:48:24 -0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, resp) => {
-    resp.send('Hello World!')
-})
+  
+  resp.send('Hello World!');
+
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
